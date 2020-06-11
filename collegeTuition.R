17f49ff1f883ce5f317d9343d1f8a27e54e0a8f5
robynferg <36768952+robynferg@users.noreply.github.com>
2020-06-11 00:40:22 -0400

    Update collegeTuition.R

--- collegeTuition.R
+++ collegeTuition.R
@@ -2,7 +2,7 @@
 ## Predict mean tuition for 2017-18 school year
 ## based on covariates from the 2016-2017 school year
 
-library(ggplot2)
+library(randomForest)
 
 ## read in college data
 collegeData = read.csv('collegeData.csv')
@@ -10,134 +10,173 @@ collegeData = read.csv('collegeData.csv')
 ## read in estimator function
 source('newEstimator.R')
 
-## apply new method and regression imputation for a simple random sample 
+#### apply our new method with OLS and random forest, standard OLS and random forest adjustment, and sample mean 
+### for a simple random sample 
 set.seed(1234)
 
 nsims = 10000
-ns = seq(100, 1000, by=25)
-outcomes = data.frame()
+ns = seq(100, 1000, by=100)
 summaryStats = data.frame()
+
+vars = c("admRate", "gradRate", "studStaffRatio", "doctoral", "master", "public")
+mu = mean(collegeData$tuition17)
+N = nrow(collegeData)
 
 for(n in ns){
   temp = data.frame()
   for(i in 1:nsims){
     # take sample
     samp = sample(1:nrow(collegeData), n, replace=FALSE)
-    # apply new estimator
-    new = newEstimator(Xsample = collegeData[samp,-c(1:4)], Ysample = collegeData$tuition17[samp],
-                       Xother = collegeData[-samp, -c(1:4)], pred.fun = 'lm')
-    # regression imputation
-    reg = lm(tuition17~., data=collegeData[samp, -c(1:2,4)])
-    regImp = mean(c(predict(reg, collegeData[-samp, -c(1:4)]), collegeData$tuition17[samp]))
+    # apply our estimator
+    # with OLS
+    newOLS = newEstimator(Xsample = collegeData[samp, vars], Ysample = collegeData$tuition17[samp],
+                       Xother = collegeData[-samp, vars], pred.fun = 'lm')
+    # with random forest
+    newRF = newEstimator(Xsample = collegeData[samp, vars], Ysample = collegeData$tuition17[samp],
+                         Xother = collegeData[-samp, vars], pred.fun = 'rf')
+    # regression adjustment
+    reg = lm(tuition17~., data=collegeData[samp, c(vars, 'tuition17')])
+    regImp = mean(c(predict(reg, collegeData[-samp, vars]), collegeData$tuition17[samp]))
+    regImpSdEst = sqrt((N-n)/(n*N*(n-2)) * sum(reg$residuals^2))
+    # random forest adjustment
+    rf = randomForest(tuition17~., data=collegeData[samp, c(vars, 'tuition17')])
+    rfImp = mean(c(predict(rf, collegeData[-samp, vars]), collegeData$tuition17[samp]))
+    rfImpSdEst = sqrt((N-n)/(n*N*(n-2)) * sum((collegeData$tuition17[samp]-rf$predicted)^2))
     # sample mean
     sampMean = mean(collegeData$tuition17[samp])
+    sampMeanEstSd = (N-n)/(n*N*(n-1)) * sum((collegeData$tuition17[samp]-sampMean)^2)
     # save outcomes
-    temp = rbind(temp, data.frame('n'=n, 'new'=new$muHat, 'newVarEst'=new$varEst,
-                                  'regImp'=regImp, 'sampMean'=sampMean))
+    temp = rbind(temp, data.frame('n'=n, 'newOLS'=newOLS$muHat, 'newOLSVarEst'=newOLS$varEst,
+                                  'newRF'=newRF$muHat, 'newRFVarEst'=newRF$varEst,
+                                  'regImp'=regImp, 'regImpSdEst'=regImpSdEst,
+                                  'rfImp'=rfImp, 'rfImpSdEst'=rfImpSdEst, 
+                                  'sampMean'=sampMean, 'sampMeanSdEst'=sampMeanEstSd))
   }
   # save summary statistics
-  summaryStats = rbind(summaryStats, data.frame('n'=n, 'newMean'=mean(temp$new), 'newSE'=sd(temp$new), 'newVarEst'=mean(temp$newVarEst),
-                       'regImpMean'=mean(temp$regImp), 'regImpSE'=sd(temp$regImp),
-                       'sampMeanMean'=mean(temp$sampMean), 'sampMeanSE'=sd(temp$sampMean)))
-  outcomes = rbind(outcomes, temp)
-  #print(n)
+  summary = data.frame('n'=n, 'newMeanOLS'=mean(temp$newOLS), 'newSEOLS'=sd(temp$newOLS), 
+                       'newVarEstOLS'=mean(temp$newOLSVarEst), 'newMSEOLS'=mean((temp$newOLS-mu)^2),
+                       'newMeanRF'=mean(temp$newRF), 'newSERF'=sd(temp$newRF), 
+                       'newVarEstRF'=mean(temp$newRFVarEst), 'newMSERF'=mean((temp$newRF-mu)^2),
+                       'regImpMean'=mean(temp$regImp), 'regImpSE'=sd(temp$regImp), 
+                       'regImpSdEst'=mean(temp$regImpSdEst), 'regImpMSE'=mean((temp$regImp-mu)^2),
+                       'rfImpMean'=mean(temp$rfImp), 'rfImpSE'=sd(temp$rfImp),
+                       'rfImpSdEst'=mean(temp$rfImpSdEst), 'rfImpMSE'=mean((temp$rfImp-mu)^2),
+                       'sampMeanMean'=mean(temp$sampMean), 'sampMeanSE'=sd(temp$sampMean), 
+                       'sampMeanSdEst'=mean(temp$sampMeanSdEst), 'sampMeanMSE'=mean((temp$sampMean-mu)^2))
+  summaryStats = rbind(summaryStats, summary)
+  print(n)
 }
 
 
-## plot results
-  # bias for new method and imputation
-mu = mean(collegeData$tuition17)
-
-summaryStats$newBias = mu - summaryStats$newMean
-summaryStats$regImpBias = mu - summaryStats$regImpMean
-summaryStats$sampMeanBias = mu - summaryStats$sampMeanMean
-
-summaryStats.gg = data.frame('n'=summaryStats$n, 'Bias'=summaryStats$newBias, 
-                             'UpperCI'=summaryStats$newBias+2*summaryStats$newSE/sqrt(nsims),
-                             'LowerCI'=summaryStats$newBias-2*summaryStats$newSE/sqrt(nsims),
-                             'Method'='New')
-summaryStats.gg = rbind(summaryStats.gg, data.frame('n'=summaryStats$n, 'Bias'=summaryStats$regImpBias,
-                                                    'UpperCI'=summaryStats$regImpBias+2*summaryStats$regImpSE/sqrt(nsims),
-                                                    'LowerCI'=summaryStats$regImpBias-2*summaryStats$regImpSE/sqrt(nsims),
-                                                    'Method'='Imputation'))
-pdf('college_SRSbias.pdf')
-ggplot(summaryStats.gg, aes(n, Bias, colour=Method)) + 
-  geom_ribbon(aes(ymin=LowerCI, ymax=UpperCI), fill='grey80', alpha=.3) + geom_hline(yintercept=0) +
-  geom_point(aes(colour=Method)) + geom_line() + theme_bw() + scale_colour_grey()
-dev.off()
-
-  # SE comparison for new method and sample mean
-miny = with(summaryStats, min(newSE, regImpSE, sampMeanSE))
-maxy = with(summaryStats, max(newSE, regImpSE, sampMeanSE))
-pdf('college_SRSse.pdf')
-plot(summaryStats$n, summaryStats$newSE, ylim=c(miny, maxy), pch=19, 
-     main='Simulated Standard Error', xlab='Sample Size', ylab='SE')
-points(summaryStats$n, summaryStats$sampMeanSE, col='grey', pch=19)
-legend('topright', pch=c(19,19), col=c('black', 'grey'), legend=c('New method', 'sample mean'))
-dev.off()
-
-  # MSE
-mu = mean(collegeData$tuition17)
-summaryStats$newMSE = NA
-summaryStats$regImpMSE = NA
-summaryStats$sampMeanMSE = NA
-for(n in summaryStats$n){
-  nsubset = outcomes[outcomes$n==n,]
-  summaryStats$newMSE[which(summaryStats$n==n)] = 1/nsims * sum((nsubset$new-mu)^2)
-  summaryStats$regImpMSE[which(summaryStats$n==n)] = 1/nsims * sum((nsubset$regImp-mu)^2)
-  summaryStats$sampMeanMSE[which(summaryStats$n==n)] = 1/nsims * sum((nsubset$sampMean-mu)^2)
-}
-
-pdf('college_SRSmse.pdf')
-plot(summaryStats$n, summaryStats$regImpMSE - summaryStats$newMSE, pch=19,
-     main="Difference in MSE between New Method and Regression Imputation", 
-     xlab="Sample Size", ylab="MSE(Regression Imputation) - MSE(New)")
-dev.off()
+## results of SRS
+srs_table = with(summaryStats, cbind(n, 'sampMean_TrueSE'=sampMeanSE, 'sampMean_EstSE'=sqrt(sampMeanSdEst), 
+                                     'sampMean_Bias'=mu-sampMeanMean, 'sampMean_t'=(mu-sampMeanMean)/(sampMeanSE/sqrt(nsims)),
+                                     'OLSadj_TrueSE'=regImpSE, 'OLSadj_EstSE'=regImpSdEst,
+                                     'OLSadj_Bias'=mu-regImpMean, 'OLSadj_t'=(mu-regImpMean)/(regImpSE/sqrt(nsims)),
+                                     'NewOLS_TrueSE'=newSEOLS, 'NewOLS_EstSE'=sqrt(newVarEstOLS),
+                                     'NewOLS_Bias'=mu-newMeanOLS, 'NewOLS_t'=(mu-newMeanOLS)/(newSEOLS/sqrt(nsims)),
+                                     'RFadj_TrueSE'=rfImpSE, 'RFadj_EstSE'=rfImpSdEst,
+                                     'RFadj_Bias'=mu-rfImpMean, 'RFadj_t'=(mu-rfImpMean)/(rfImpSE/sqrt(nsims)),
+                                     'NewRF_TrueSE'=newSERF, 'NewRF_EstSE'=sqrt(newVarEstRF),
+                                     'NewRF_Bias'=mu-newMeanRF, 'NewRF_t'=(mu-newMeanRF)/(newSERF/sqrt(nsims))))
+print(round(srs_table, 2))
 
 
+## Appendix table
+srs_appendix = with(summaryStats, cbind(n,'newOLSSE'=newSEOLS, 'newOLSbias'=mu-newMeanOLS, 
+                                      'adjOLSSE'=regImpSE, 'adjOLSbias'=mu-regImpMean,
+                                      'nweRFSE'=newSERF, 'newRFbias'=mu-newMeanRF, 
+                                      'adjRFSE'=rfImpSE, 'adjRFbias'=mu-rfImpMean,
+                                      'sampMeanSE'=sampMeanSE, 'sampMeanBias'=mu-sampMeanMean))
+print(round(srs_appendix, 2))
 
-##### Non-probability sample
+
+### Non-probability sample
 set.seed(2345)
 
 nsims = 10000
 ns = seq(100, 1000, by=100)
-outcomes.nonprob = data.frame()
 summaryStats.nonprob = data.frame()
 
 # sampling probabilities: college with highest tuition 2x as likely to be chosen
 lm.prob = lm(c(1,2)~c(min(collegeData$tuition16), max(collegeData$tuition16)))
 samp.probs = lm.prob$coef[1] + lm.prob$coef[2]*collegeData$tuition16
 
+
 for(n in ns){
   temp = data.frame()
   for(i in 1:nsims){
     # take sample
     samp = sample(1:nrow(collegeData), n, replace=FALSE, prob=samp.probs)
-    # apply new estimator
-    new = newEstimator(Xsample = collegeData[samp,-c(1:4)], Ysample = collegeData$tuition17[samp],
-                       Xother = collegeData[-samp, -c(1:4)], pred.fun = 'lm')
-    # regression imputation
-    reg = lm(tuition17~., data=collegeData[samp, -c(1:2,4)])
-    regImp = mean(c(predict(reg, collegeData[-samp, -c(1:4)]), collegeData$tuition17[samp]))
+    # apply our estimator
+    # with OLS
+    newOLS = newEstimator(Xsample = collegeData[samp, vars], Ysample = collegeData$tuition17[samp],
+                          Xother = collegeData[-samp, vars], pred.fun = 'lm')
+    # with random forest
+    newRF = newEstimator(Xsample = collegeData[samp, vars], Ysample = collegeData$tuition17[samp],
+                         Xother = collegeData[-samp, vars], pred.fun = 'rf')
+    # regression adjustment
+    reg = lm(tuition17~., data=collegeData[samp, c(vars, 'tuition17')])
+    regImp = mean(c(predict(reg, collegeData[-samp, vars]), collegeData$tuition17[samp]))
+    regImpSdEst = sqrt((N-n)/(n*N*(n-2)) * sum(reg$residuals^2))
+    # random forest adjustment
+    rf = randomForest(tuition17~., data=collegeData[samp, c(vars, 'tuition17')])
+    rfImp = mean(c(predict(rf, collegeData[-samp, vars]), collegeData$tuition17[samp]))
+    rfImpSdEst = sqrt((N-n)/(n*N*(n-2)) * sum((collegeData$tuition17[samp]-rf$predicted)^2))
     # sample mean
     sampMean = mean(collegeData$tuition17[samp])
+    sampMeanEstSd = (N-n)/(n*N*(n-1)) * sum((collegeData$tuition17[samp]-sampMean)^2)
     # save outcomes
-    temp = rbind(temp, data.frame('n'=n, 'new'=new$muHat, 'newVarEst'=new$varEst,
-                                  'regImp'=regImp, 'sampMean'=sampMean))
+    temp = rbind(temp, data.frame('n'=n, 'newOLS'=newOLS$muHat, 'newOLSVarEst'=newOLS$varEst,
+                                  'newRF'=newRF$muHat, 'newRFVarEst'=newRF$varEst,
+                                  'regImp'=regImp, 'regImpSdEst'=regImpSdEst,
+                                  'rfImp'=rfImp, 'rfImpSdEst'=rfImpSdEst, 
+                                  'sampMean'=sampMean, 'sampMeanSdEst'=sampMeanEstSd))
   }
   # save summary statistics
-  summaryStats.nonprob = rbind(summaryStats.nonprob, data.frame('n'=n, 'newMean'=mean(temp$new), 'newSE'=sd(temp$new), 'newVarEst'=mean(temp$newVarEst),
-                                                'regImpMean'=mean(temp$regImp), 'regImpSE'=sd(temp$regImp),
-                                                'sampMeanMean'=mean(temp$sampMean), 'sampMeanSE'=sd(temp$sampMean)))
-  outcomes.nonprob = rbind(outcomes.nonprob, temp)
-  #print(n)
+  summary = data.frame('n'=n, 'newMeanOLS'=mean(temp$newOLS), 'newSEOLS'=sd(temp$newOLS), 
+                       'newVarEstOLS'=mean(temp$newOLSVarEst), 'newMSEOLS'=mean((temp$newOLS-mu)^2),
+                       'newMeanRF'=mean(temp$newRF), 'newSERF'=sd(temp$newRF), 
+                       'newVarEstRF'=mean(temp$newRFVarEst), 'newMSERF'=mean((temp$newRF-mu)^2),
+                       'regImpMean'=mean(temp$regImp), 'regImpSE'=sd(temp$regImp), 
+                       'regImpSdEst'=mean(temp$regImpSdEst), 'regImpMSE'=mean((temp$regImp-mu)^2),
+                       'rfImpMean'=mean(temp$rfImp), 'rfImpSE'=sd(temp$rfImp),
+                       'rfImpSdEst'=mean(temp$rfImpSdEst), 'rfImpMSE'=mean((temp$rfImp-mu)^2),
+                       'sampMeanMean'=mean(temp$sampMean), 'sampMeanSE'=sd(temp$sampMean), 
+                       'sampMeanSdEst'=mean(temp$sampMeanSdEst), 'sampMeanMSE'=mean((temp$sampMean-mu)^2))
+  summaryStats.nonprob = rbind(summaryStats.nonprob, summary)
+  print(n)
 }
 
-# table of bias
-print(with(summaryStats.nonprob, round(data.frame('n'=n, 'newLowerCI'=newMean-2*newSE/sqrt(nsims)-mean(collegeData$tuition17), 
-                                 'newUpperCI'=newMean+2*newSE/sqrt(nsims)-mean(collegeData$tuition17),
-                                 'regImpLowerCI'=regImpMean-2*regImpSE/sqrt(nsims)-mean(collegeData$tuition17),
-                                 'regImpUpperCI'=regImpMean+2*regImpSE/sqrt(nsims)-mean(collegeData$tuition17),
-                                 'meanLowerCI'=sampMeanMean-2*sampMeanSE/sqrt(nsims)-mean(collegeData$tuition17),
-                                 'meanUpperCI'=sampMeanMean+2*sampMeanSE/sqrt(nsims)-mean(collegeData$tuition17)), 2)))
 
+## results of non-SRS
+nonsrs_table = with(summaryStats.nonprob, cbind(n, 'sampMean_TrueSE'=sampMeanSE, 'sampMean_EstSE'=sqrt(sampMeanSdEst), 
+                                     'sampMean_Bias'=mu-sampMeanMean, 'sampMean_t'=(mu-sampMeanMean)/(sampMeanSE/sqrt(nsims)),
+                                     'OLSadj_TrueSE'=regImpSE, 'OLSadj_EstSE'=regImpSdEst,
+                                     'OLSadj_Bias'=mu-regImpMean, 'OLSadj_t'=(mu-regImpMean)/(regImpSE/sqrt(nsims)),
+                                     'NewOLS_TrueSE'=newSEOLS, 'NewOLS_EstSE'=sqrt(newVarEstOLS),
+                                     'NewOLS_Bias'=mu-newMeanOLS, 'NewOLS_t'=(mu-newMeanOLS)/(newSEOLS/sqrt(nsims)),
+                                     'RFadj_TrueSE'=rfImpSE, 'RFadj_EstSE'=rfImpSdEst,
+                                     'RFadj_Bias'=mu-rfImpMean, 'RFadj_t'=(mu-rfImpMean)/(rfImpSE/sqrt(nsims)),
+                                     'NewRF_TrueSE'=newSERF, 'NewRF_EstSE'=sqrt(newVarEstRF),
+                                     'NewRF_Bias'=mu-newMeanRF, 'NewRF_t'=(mu-newMeanRF)/(newSERF/sqrt(nsims))))
+print(round(nonsrs_table, 2))
+
+
+## Appendix table
+nonsrs.nonprob = with(summaryStats.nonprob, cbind(n,'newOLSSE'=newSEOLS, 'newOLSbias'=mu-newMeanOLS, 
+                                      'adjOLSSE'=regImpSE,'adjOLSbias'=mu-regImpMean, 
+                                      'nweRFSE'=newSERF,'newRFbias'=mu-newMeanRF, 
+                                      'adjRFSE'=rfImpSE,'adjRFbias'=mu-rfImpMean, 
+                                      'sampMeanSE'=sampMeanSE, 'sampMeanbias'=mu-sampMeanMean))
+print(round(nonsrs.nonprob, 2))
+
+
+
+#### OLS using all data
+allOLS = lm(tuition17~admRate + gradRate + studStaffRatio + doctoral + master + public, data = collegeData)
+plot(allOLS)
+
+# outliers: Brigham Young University and Inter American University of Puerto Rico
+collegeData[1106:1107,]
+collegeData[1233:1235,]
